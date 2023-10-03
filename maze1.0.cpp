f31cbbe31b7fd5219d5aed83d5c20952179608fc
AlirezaNoorizadeh <alirezanoorizadeh82@gmail.com>
2023-10-03 23:11:14 +0330

    maze 1.2 : Removing the DFS algorithm / rewriting the finisher function / rewriting the ifs in the generateMaze function and other minor changes / removing the condition of checking the wall from main / adding the display section of the game duration to main

--- maze1.0.cpp
+++ maze1.0.cpp
@@ -10,53 +10,47 @@ const int WALL = 0;
 const int PATH = 1;
 const int START = 2;
 const int END = 3;
-const int MAX_MOVES = 1000;
+const int MAX_MOVES = 150;
 
 int maze[ROW][COL];
 
-bool dfs(int row, int col) {
-    if (row < 0 || row >= ROW || col < 0 || col >= COL || maze[row][col] == WALL) {
-        return false;
-    }
-    if (maze[row][col] == END) {
-        return true;
-    }
-    maze[row][col] = WALL; // Mark as visited
-    bool result = dfs(row - 1, col) || dfs(row + 1, col) || dfs(row, col - 1) || dfs(row, col + 1);
-    maze[row][col] = PATH; // Unmark as visited
-    return result;
-}
-
 int boolian = 1;
 
+// void finisher(int currentRow, int currentCol){
+//          if(currentRow == 13 && currentCol == 11)    maze[13][12] = PATH;
+//     else if(currentRow == 12 && currentCol == 11)    maze[12][12] = maze[13][12] = PATH;
+//     else if(currentRow == 11 && currentCol == 11)    maze[12][11] = maze[12][12] = maze[13][12] = PATH;
+//     else if(currentRow == 11 && currentCol == 12)    maze[12][12] = maze[12][13] = PATH;
+//     else if(currentRow == 11 && currentCol == 13)    maze[12][13] = PATH;
+//     boolian = 0;
+// }
+
 void finisher(int currentRow, int currentCol){
-            if(currentRow == 13 && currentCol == 11)    maze[13][12] = PATH;
-    else if(currentRow == 12 && currentCol == 11)    maze[12][12] = maze[13][12] = PATH;
-    else if(currentRow == 11 && currentCol == 11)    maze[12][11] = maze[12][12] = maze[13][12] = PATH;
-    else if(currentRow == 11 && currentCol == 12)    maze[12][12] = maze[12][13] = PATH;
-    else if(currentRow == 11 && currentCol == 13)    maze[12][13] = PATH;
+         if(currentRow == ROW-2 && currentCol == COL-4)    maze[ROW-2][COL-3] = PATH;
+    else if(currentRow == ROW-3 && currentCol == COL-4)    maze[ROW-3][COL-3] = maze[ROW-2][COL-3] = PATH;
+    else if(currentRow == ROW-4 && currentCol == COL-4)    maze[ROW-3][COL-4] = maze[ROW-3][COL-3] = maze[ROW-2][COL-3] = PATH;
+    else if(currentRow == ROW-4 && currentCol == COL-3)    maze[ROW-3][COL-3] = maze[ROW-3][COL-2] = PATH;
+    else if(currentRow == ROW-4 && currentCol == COL-2)    maze[ROW-3][COL-2] = PATH;
     boolian = 0;
 }
 
 void generateMaze() {
-    // Initialize maze with walls
+    // maze walls
     for (int i = 0; i < ROW; i++) {
         for (int j = 0; j < COL; j++) {
             maze[i][j] = WALL;
         }
     }
 
-    // Set start and end points
     maze[1][1] = START;
     maze[13][13] = END;
 
-    
-    // Generate random path
-    int currentRow = 1; //radis
+    // masir = path
+    int currentRow = 1; //radif
     int currentCol = 1; //soton
     int moves = 0;
-    
-    while ( (currentRow != ROW - 2 || currentCol != COL - 2) && moves < MAX_MOVES && boolian == 0) {
+
+    while (boolian == 1) {
         int direction = rand() % 4;
         switch(direction) {
             case 0: // Move up
@@ -65,8 +59,11 @@ void generateMaze() {
                     maze[currentRow - 2][currentCol] = PATH;
                     currentRow -= 2;
                 }
-                if(currentRow >= 11 || currentCol >= 11){
-                    finisher(currentRow,currentCol) ;  
+                // if(currentRow >= 11 && currentCol >= 11){
+                //     finisher(currentRow,currentCol) ;  
+                // }
+                if(currentRow >= ROW-4 && currentCol >= COL-4){
+                    finisher(currentRow,currentCol) ;
                 }
                 break;
             case 1: // Move down
@@ -75,8 +72,8 @@ void generateMaze() {
                     maze[currentRow + 2][currentCol] = PATH;
                     currentRow += 2;
                 }
-                if(currentRow >= 11 || currentCol >= 11){
-                    finisher(currentRow,currentCol) ;  
+                if(currentRow >= ROW - 4 && currentCol >= COL-4){
+                    finisher(currentRow,currentCol) ;
                 }
                 break;
             case 2: // Move left
@@ -85,8 +82,8 @@ void generateMaze() {
                     maze[currentRow][currentCol - 2] = PATH;
                     currentCol -= 2;
                 }
-                if(currentRow >= 11 || currentCol >= 11){
-                    finisher(currentRow,currentCol) ;  
+                if(currentRow >= ROW-4 && currentCol >= COL-4){
+                    finisher(currentRow,currentCol) ;
                 }
                 break;
             case 3: // Move right
@@ -95,98 +92,25 @@ void generateMaze() {
                     maze[currentRow][currentCol + 2] = PATH;
                     currentCol += 2;
                 }
-                if(currentRow >= 11 || currentCol >= 11){
-                    finisher(currentRow,currentCol) ;  
+                if(currentRow >= ROW-4 && currentCol >= COL-4){
+                    finisher(currentRow,currentCol) ;
                 }
                 break;
         }
         moves++;
-        // if(moves < MAX_MOVES){
-        //     generateMaze();
-        // }
-    }    
-
-    // while(1){
-    //     int direct = rand() % 4;
-
-    // }
-    // while(1){
-    //     // int direct = rand() % 4;
-    //     int temp=(rand())%4+1; 
-
-    //     if(temp==1) currentCol--; // حرکت به بالا
-    //     else if(temp==2) currentRow++; // حرکت به راست
-    //     else if(temp==3) currentCol++; // حرکت به پایین
-    //     else if(temp==4) currentRow--; // حرکت به چپ
-
-    //     // بررسی اینکه آیا خارج از محدوده نقشه رفته‌ایم یا نه و در صورت لزوم، بازگشت به محدوده مناسب
-    //     if(currentCol==0) currentCol++;
-    //     else if(currentCol==14) currentCol--;
-    //     else if(currentRow==0) currentRow++;
-    //     else if(currentRow==14) currentRow--;
-
-    //     if(maze[currentCol][currentRow] == 3) break; // اگر به سوراخ بزرگ رسیدیم، حلقه while را قطع کن
-    //     if(maze[currentCol][currentRow] == 2) continue; // اگر به نقطه شروع رسیدیم، هیچ کاری نکن و ادامه بده
-
-    //     maze[currentCol][currentRow] = 1; // اگر به خانه‌ جدید رسیدیم، آن را به عنوان مسیر نشان بده
-    //     moves++;
-
-    //     if(moves > 60){ // اگر تعداد تلاش‌ها بیشتر از ۶۰ شد، نقشه را صفر کن و از اول شروع کن 
-    //         for(int i = 0; i < 15; i++){
-    //             for(int j = 0; j < 15; j++){
-    //                 maze[i][j] = 0;
-    //             }
-    //         }
-    //         maze[13][13] = 3;
-    //         maze[1][1] = 2;
-
-    //         currentRow = 1, currentCol = 1;
-    //         moves = 0;
-    //     }
-    // }
-
-    // while (/*maze[currentRow][currentCol] != 3*/ (currentRow != ROW - 2 || currentCol != COL - 2) && moves < MAX_MOVES) {
-    // // while (maze[currentRow][currentCol] != END) {
-    //     int direction = rand() % 4;
-    //     switch(direction) {
-    //         case 0: // Move up
-    //             if (currentRow > 1 && maze[currentRow - 2][currentCol] == WALL) {
-    //                 maze[currentRow - 1][currentCol] = PATH;
-    //                 // maze[currentRow - 2][currentCol] = PATH;
-    //                 // currentRow -= 2;
-    //                 currentRow -= 1;
-    //             }
-    //             break;
-    //         case 1: // Move down
-    //             if (currentRow < ROW - 2 && maze[currentRow + 2][currentCol] == WALL) {
-    //                 maze[currentRow + 1][currentCol] = PATH;
-    //                 // maze[currentRow + 2][currentCol] = PATH;
-    //                 // currentRow += 2;
-    //                 currentRow += 1;
-    //             }
-    //             break;
-    //         case 2: // Move left
-    //             if (currentCol > 1 && maze[currentRow][currentCol - 2] == WALL) {
-    //                 maze[currentRow][currentCol - 1] = PATH;
-    //                 // maze[currentRow][currentCol - 2] = PATH;
-    //                 // currentCol -= 2;
-    //                 currentCol -= 1;
-    //             }
-    //             break;
-    //         case 3: // Move right
-    //             if (currentCol < COL - 2 && maze[currentRow][currentCol + 2] == WALL) {
-    //                 maze[currentRow][currentCol + 1] = PATH;
-    //                 // maze[currentRow][currentCol + 2] = PATH;
-    //                 // currentCol += 2;
-    //                 currentCol += 1;
-    //             }
-    //             break;
-    //     }
-    //     moves++;
-    //     // if(moves < MAX_MOVES){
-    //     //     generateMaze();
-    //     // }
-    // }
+        if(moves > MAX_MOVES){
+            for (int i = 0; i < ROW; i++) {
+                for (int j = 0; j < COL; j++) {
+                    maze[i][j] = WALL;
+                }
+            }
+            maze[1][1] = START;
+            maze[13][13] = END;
+            currentRow = 1; //radif
+            currentCol = 1; //soton
+            moves = 0;
+        }
+    }
 }
 
 void printMaze(int currentRow, int currentCol) {
@@ -215,70 +139,60 @@ void printMaze(int currentRow, int currentCol) {
     }
 }
 
-bool isWall(int row, int col) {
-    return (row == 0 || col == 0 || row == ROW-1 || col == COL-1);
-}
-
 int main() {
     srand(time(NULL));
+    // cout << time(0) << endl;
+    // cout << time(NULL) << endl;
     generateMaze();
-    
-    while(!dfs(1,1)){
-        generateMaze();
-    }
+    int startTime = time(0);
     int currentRow = 1;
     int currentCol = 1;
+    int moves =0;
 
     while (maze[currentRow][currentCol] != END) {
         system("cls");
         printMaze(currentRow, currentCol);
-
+        
         char move;
-        // cout << "Enter a move (u/d/l/r): ";
         int arrow_key = getch();
-             if(arrow_key == 80) move = 'd';
-        else if(arrow_key == 72) move = 'u';
-        else if(arrow_key == 77) move = 'r';
-        else if(arrow_key == 75) move = 'l';
-        // cin >> move;
+             if(arrow_key == 80 || arrow_key == 's') move = 'd';
+        else if(arrow_key == 72 || arrow_key == 'w') move = 'u';
+        else if(arrow_key == 77 || arrow_key == 'd') move = 'r';
+        else if(arrow_key == 75 || arrow_key == 'a') move = 'l';
 
         switch(move) {
             case 'u':
                 if (currentRow > 1 && maze[currentRow - 1][currentCol] != WALL) {
                     currentRow--;
+                    moves++;
                 }
                 break;
             case 'd':
                 if (currentRow < ROW - 2 && maze[currentRow + 1][currentCol] != WALL) {
                     currentRow++;
+                    moves++;
                 }
                 break;
             case 'l':
                 if (currentCol > 1 && maze[currentRow][currentCol - 1] != WALL) {
                     currentCol--;
+                    moves++;
                 }
                 break;
             case 'r':
                 if (currentCol < COL - 2 && maze[currentRow][currentCol + 1] != WALL) {
                     currentCol++;
+                    moves++;
                 }
                 break;
             default:
                 cout << "Invalid move. Try again." << endl;
             continue;
+        }
     }
+    int endTime = time(0) - startTime;
+    printMaze(currentRow, currentCol);
+    cout << "\n**** You won this game in " << endTime << " seconds and with " << moves << " moves! ****" << endl;
 
-    if (isWall(currentRow, currentCol)) {
-        // Player hit a wall, reset maze
-        cout << "You hit a wall! Maze reset." << endl;
-        generateMaze();
-        currentRow = 1;
-        currentCol = 1;
-    }
-}
-// Player reached the end, game over
-printMaze(currentRow, currentCol);
-cout << "Congratulations, you made it to the end!" << endl;
-
-return 0;
+    return 0;
 }
