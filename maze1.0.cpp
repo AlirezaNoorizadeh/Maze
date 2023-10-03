58c8df815bd0db8feef4695044d273e43a26503b
AlirezaNoorizadeh <alirezanoorizadeh82@gmail.com>
2023-10-04 01:24:45 +0330

    maze 1.14 : forward declaration of all functions

--- maze1.0.cpp
+++ maze1.0.cpp
@@ -30,6 +30,126 @@ void setColor(int color) {
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
 }
 
+void saveScores();
+void fileReader();
+void Rename();
+void mainMenu();
+void colorSeter();
+void setDifficulty();
+bool compareUsers(User a, User b);
+void printMaze(int currentRow, int currentCol,int ROW, int COL, int wallColor, int cursorColor);
+void generateMaze(int row, int col);
+
+int main() {
+    srand(time(NULL));
+    fileReader();
+    
+    while(1){
+        mainMenu();
+        system("cls");
+        generateMaze(ROW,COL);
+        startTime = time(0);
+        currentRow = 1;
+        currentCol = 1;
+        moves = 0;
+
+        while (maze[currentRow][currentCol] != END) {
+            system("cls");
+            printMaze(currentRow, currentCol, ROW, COL, wallColor, cursorColor);
+            char move;
+            int arrow_key = getch();
+                 if(arrow_key == 'x' || arrow_key == 's' || arrow_key== '2' || arrow_key=='5' || arrow_key == 80) move = 'd';
+            else if(arrow_key == 'w' || arrow_key == '8' || arrow_key == 72) move = 'u';
+            else if(arrow_key == 'd' || arrow_key == '6' || arrow_key == 77) move = 'r';
+            else if(arrow_key == 'a' || arrow_key == '4' || arrow_key == 75) move = 'l';
+            else if(arrow_key == 'q' || arrow_key == '7' ) move = 'q'; //u+l
+            else if(arrow_key == 'e' || arrow_key == '9' ) move = 'e'; //u+r
+            else if(arrow_key == 'z' || arrow_key == '1' ) move = 'z'; //d+l
+            else if(arrow_key == 'c' || arrow_key == '3' ) move = 'c'; //d+r
+            fflush(stdin);
+
+            switch(move) {
+                case 'u': 
+                    if (currentRow > 1 && maze[currentRow - 1][currentCol] != WALL) { currentRow--; moves++; }
+                    break;
+                case 'd':
+                    if (currentRow < ROW - 2 && maze[currentRow + 1][currentCol] != WALL) { currentRow++; moves++; }
+                    break;
+                case 'l':
+                    if (currentCol > 1 && maze[currentRow][currentCol - 1] != WALL) { currentCol--; moves++; }
+                    break;
+                case 'r':
+                    if (currentCol < COL - 2 && maze[currentRow][currentCol + 1] != WALL) { currentCol++; moves++; }
+                    break;
+                case 'q': //u+l
+                    if (currentRow > 1 && currentCol > 1 && maze[currentRow-1][currentCol-1] != WALL) { currentRow--; currentCol--; moves++; }
+                    break;
+                case 'e': //u+r
+                    if (currentRow > 1 && currentCol < COL - 2 && maze[currentRow-1][currentCol+1] != WALL) { currentRow--; currentCol++; moves++; }
+                    break;
+                case 'z': //d+l
+                    if (currentRow < ROW - 2 && currentCol > 1 && maze[currentRow+1][currentCol-1] != WALL) { currentRow++; currentCol--; moves++; }
+                    break; 
+                case 'c': //dr
+                    if (currentRow < ROW - 2 && currentCol < COL - 2 && maze[currentRow+1][currentCol+1] != WALL) { currentRow++; currentCol++; moves++; }
+                    break; 
+                default:
+                    cout << "Invalid move. Try again." << endl;
+                    continue;
+            }
+        }
+
+        endTime = time(0);
+        lengthTime = endTime - startTime;
+        system("cls");
+        cout << "\n **** You won this game in " << lengthTime << " seconds and with " << moves << " moves! ****\n Press any key to continue..." << getch() << endl;
+        fflush(stdin);
+        system("cls");
+        cout << "\n(Your score = The duration of the game*2 + the number of moves you have made in the game/2)\n Score : " << (lengthTime*2)+(moves/2) <<" \n Name : ";
+        
+        User newUser;
+        cin >> newUser.name;
+        newUser.score=(lengthTime*2)+(moves/2);
+        newUser.startTime=startTime;
+        newUser.endTime=endTime;
+
+        bool userExists = false;
+        for (int i = 0; i < list.size(); i++) {
+            if (list[i].name == newUser.name) {
+                userExists = true;
+                if (newUser.score < list[i].score) {
+                    list[i].score = newUser.score;
+                    list[i].startTime = newUser.startTime;
+                    list[i].endTime = newUser.endTime;
+                }
+                setColor(2);
+                cout << " Hi " << list[i].name << " (ID = " << list[i].id << ")" << " your best score is " << list[i].score << " now.\n Press any key to continue..." << endl;
+                getch();
+                setColor(7);
+                newUser.id = list[i].id;
+                break;
+            }
+        }
+        if (!userExists) {
+            newUser.id=startTime%10000;
+            list.push_back(newUser);
+        }
+
+        sort(list.begin(), list.end(), compareUsers);
+        saveScores();
+
+        system("cls");
+        cout << "\n List of top 3 players:" << endl;
+        int numPlayers = (list.size() < 3) ? list.size() : 3;
+        for (int i = 0; i < numPlayers; i++) {
+            cout << left << setw(3) << " " << i+1 << +") " << setw(7) << list[i].name << "    score: " << setw(3) << list[i].score << "  id: " << setw(4) << list[i].id << endl;
+        }
+
+        cout << "\n Press any key to return to the menu..." << getch() << fflush(stdin); 
+    }
+    return 0;
+}
+
 void generateMaze(int row, int col) {
     int ROW = row;
     int COL = col;
@@ -196,7 +316,7 @@ void setDifficulty(){
         }
 }
 
-void setColor(){
+void colorSeter(){
     system("cls");
     for (int col_code = 1; col_code < 256; col_code++) {
         setColor(col_code);
@@ -234,7 +354,6 @@ void setColor(){
     }
 }
 
-void Rename();
 void mainMenu(){
     setColor(7);
     bool main = true;
@@ -246,7 +365,7 @@ void mainMenu(){
             case 1: main=false; break;
             case 2: Rename(); break;
             case 3: setDifficulty(); break;
-            case 4: setColor(); break;
+            case 4: colorSeter(); break;
             case 5: system("cls"); setColor(13); cout<< "\n ********** THE END **********\n\n"; exit(0); break;
             default: cout<<" this not be valid try 1-5\n press any key to continue..."; getch(); break;
         }
@@ -313,7 +432,6 @@ void fileReader(){
         fclose(file);
     }
 }
-
 void saveScores(){
     // Save scores to file
     file = fopen("scores.txt", "w");
@@ -325,116 +443,4 @@ void saveScores(){
         fprintf(file, "%s %d %d %d %d\n", list[i].name.c_str(), list[i].score, list[i].startTime, list[i].endTime, list[i].id);
     }
     fclose(file);
-}
-
-int main() {
-    srand(time(NULL));
-    fileReader();
-    
-    while(1){
-        mainMenu();
-        system("cls");
-        generateMaze(ROW,COL);
-        startTime = time(0);
-        currentRow = 1;
-        currentCol = 1;
-        moves = 0;
-
-        while (maze[currentRow][currentCol] != END) {
-            system("cls");
-            printMaze(currentRow, currentCol, ROW, COL, wallColor, cursorColor);
-
-            //move
-            char move;
-            int arrow_key = getch();
-                 if(arrow_key == 'x' || arrow_key == 's' || arrow_key== '2' || arrow_key=='5' || arrow_key == 80) move = 'd';
-            else if(arrow_key == 'w' || arrow_key == '8' || arrow_key == 72) move = 'u';
-            else if(arrow_key == 'd' || arrow_key == '6' || arrow_key == 77) move = 'r';
-            else if(arrow_key == 'a' || arrow_key == '4' || arrow_key == 75) move = 'l';
-            else if(arrow_key == 'q' || arrow_key == '7' ) move = 'q'; //u+l
-            else if(arrow_key == 'e' || arrow_key == '9' ) move = 'e'; //u+r
-            else if(arrow_key == 'z' || arrow_key == '1' ) move = 'z'; //d+l
-            else if(arrow_key == 'c' || arrow_key == '3' ) move = 'c'; //d+r
-            fflush(stdin);
-
-            switch(move) {
-                case 'u': 
-                    if (currentRow > 1 && maze[currentRow - 1][currentCol] != WALL) { currentRow--; moves++; }
-                    break;
-                case 'd':
-                    if (currentRow < ROW - 2 && maze[currentRow + 1][currentCol] != WALL) { currentRow++; moves++; }
-                    break;
-                case 'l':
-                    if (currentCol > 1 && maze[currentRow][currentCol - 1] != WALL) { currentCol--; moves++; }
-                    break;
-                case 'r':
-                    if (currentCol < COL - 2 && maze[currentRow][currentCol + 1] != WALL) { currentCol++; moves++; }
-                    break;
-                case 'q': //u+l
-                    if (currentRow > 1 && currentCol > 1 && maze[currentRow-1][currentCol-1] != WALL) { currentRow--; currentCol--; moves++; }
-                    break;
-                case 'e': //u+r
-                    if (currentRow > 1 && currentCol < COL - 2 && maze[currentRow-1][currentCol+1] != WALL) { currentRow--; currentCol++; moves++; }
-                    break;
-                case 'z': //d+l
-                    if (currentRow < ROW - 2 && currentCol > 1 && maze[currentRow+1][currentCol-1] != WALL) { currentRow++; currentCol--; moves++; }
-                    break; 
-                case 'c': //dr
-                    if (currentRow < ROW - 2 && currentCol < COL - 2 && maze[currentRow+1][currentCol+1] != WALL) { currentRow++; currentCol++; moves++; }
-                    break; 
-                default:
-                    cout << "Invalid move. Try again." << endl;
-                    continue;
-            }
-        }
-
-        endTime = time(0);
-        lengthTime = endTime - startTime;
-        system("cls");
-        cout << "\n **** You won this game in " << lengthTime << " seconds and with " << moves << " moves! ****\n Press any key to continue..." << getch() << endl;
-        fflush(stdin);
-        system("cls");
-        cout << "\n(Your score = The duration of the game*2 + the number of moves you have made in the game/2)\n Score : " << (lengthTime*2)+(moves/2) <<" \n Name : ";
-        
-        User newUser;
-        cin >> newUser.name;
-        newUser.score=(lengthTime*2)+(moves/2);
-        newUser.startTime=startTime;
-        newUser.endTime=endTime;
-
-        bool userExists = false;
-        for (int i = 0; i < list.size(); i++) {
-            if (list[i].name == newUser.name) {
-                userExists = true;
-                if (newUser.score < list[i].score) {
-                    list[i].score = newUser.score;
-                    list[i].startTime = newUser.startTime;
-                    list[i].endTime = newUser.endTime;
-                }
-                setColor(2);
-                cout << " Hi " << list[i].name << " (ID = " << list[i].id << ")" << " your best score is " << list[i].score << " now.\n Press any key to continue..." << endl;
-                getch();
-                setColor(7);
-                newUser.id = list[i].id;
-                break;
-            }
-        }
-        if (!userExists) {
-            newUser.id=startTime%10000;
-            list.push_back(newUser);
-        }
-
-        sort(list.begin(), list.end(), compareUsers);
-        saveScores();
-
-        system("cls");
-        cout << "\n List of top 3 players:" << endl;
-        int numPlayers = (list.size() < 3) ? list.size() : 3;
-        for (int i = 0; i < numPlayers; i++) {
-            cout << left << setw(3) << " " << i+1 << +") " << setw(7) << list[i].name << "    score: " << setw(3) << list[i].score << "  id: " << setw(4) << list[i].id << endl;
-        }
-
-        cout << "\n Press any key to return to the menu..." << getch() << fflush(stdin); 
-    }
-    return 0;
 }
